565bf0fc23eabe875eb6e327e939caa45c2d5f04
yngwie74 <alfredo_chv@yahoo.com>
2017-12-16 00:15:19 -0600

    Se permite especificar el universo dentro del que se buscaran similares. Se establece un umbral para las coincidencias encontradas.

--- find_closest.py
+++ find_closest.py
@@ -12,6 +12,7 @@ from scipy.misc import imread, imresize
 from scipy import average
 
 _TARGET_SIZE = [160, 200]  # [640, 800]
+_THRESHOLD = 0.50
 
 
 def prnt_stderr(s):
@@ -34,11 +35,19 @@ def read_image(filename):
                     interp='bicubic', mode='F')
 
 
+def are_versions_of_same(file1, file2):
+    if file1 == file2:
+        return True
+    no_ext = lambda fname: path.splitext(fname)[0]
+    clean_it = lambda fname: re.sub('v\\d+$', '.', no_ext(fname))
+    return clean_it(file1) == clean_it(file2)
+
+
 def compare(img1, img2):
     return 1 - ssim(img1, img2, data_range=img2.max() - img2.min())
 
 
-def main(file1):
+def main(file1, others):
     if not path.exists(file1):
         prnt_stderr("%r no existe!\n" % file1)
         exit(2)
@@ -53,9 +62,7 @@ def main(file1):
 
     img_map = dict()
 
-    all_images = (img for img
-                  in sorted(glob('*.[JjPp][PpNn][Gg]'))
-                  if img != file1)
+    all_images = (img for img in others if not are_versions_of_same(img, file1))
 
     for file2 in all_images:
         if not (path.exists(file1) and path.exists(file2)):
@@ -65,7 +72,7 @@ def main(file1):
 
         try:
             img2 = read_image(file2)
-            n_0 = compare(img1, img2)
+            n_0 = 1.0 - compare(img1, img2)
         except:
             prnt_stderr('!')
             continue
@@ -74,15 +81,26 @@ def main(file1):
 
     prnt_stderr('\n')
 
-    top_scores = list(sorted(img_map.items(), key=operator.itemgetter(1)))[:10]
+    top_scores = list((key, value)
+        for (key, value) in reversed(sorted(img_map.items(), key=operator.itemgetter(1)))
+        if value >= _THRESHOLD)[:10]
+
     for (key, value) in top_scores:
         print '\t%-4.2f\t%s:' % (value, key)
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        for arg in sys.argv[1:]:
-            main(arg)
-            print ''
-        exit(0)
+    if len(sys.argv) <= 1:
+        prnt_stderr('%s <file1> [<file2>...]' % sys.argv[0])
+        exit(1)
 
-    prnt_stderr('%s <file1> [<file2>...]' % sys.argv[0])
+    args = sys.argv[1:]
+    if ('--among' in args):
+        pos = args.index('--among')
+        others = args[pos:]
+        args = args[0:pos]
+    else:
+        others = sorted(glob('*.[JjPp][PpNn][Gg]'))
+
+    for arg in args:
+        main(arg, others)
+        print ''
