4193303d49cdb9895685d99deb51ddb81f0711df
yngwie74 <alfredo_chv@yahoo.com>
2017-12-16 00:17:07 -0600

    Permitir especificar modo inclusivo explicitamente. Por default apagado.

--- mk_cbr.sh
+++ mk_cbr.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+if [[ "$1" = "-i" ]]; then
+    _inclusive_mode="true"
+    shift
+fi
+
 if [[ $# -eq 0 ]]; then
-    echo "$0 <girl name>"
+    echo "$0 [-i] <girl name>"
     exit 1
 fi
 
-find . -maxdepth 1 -type f -name "*${1}*.??g" | sort > _list_
+# assume "inclusive mode"
+_fspec="*${1}*.??g"
+if [[ -z ${_inclusive_mode} ]]; then
+    _fspec="${1}-*.??g"
+fi
+
+find . -maxdepth 1 -type f -name "${_fspec}" | sort > _list_
 rar a -k -t "${1}.cbr" @_list_
 if [[ $? -eq 0 ]]; then
     rm _list_
