74d8b65ca0a8d14bd7d4067186d7c9fb5d00d87f
yngwie74 <alfredo_chv@yahoo.com>
2017-12-18 13:40:09 -0600

    Permitir que se ejecute desde una ruta relativa

--- find_dups.sh
+++ find_dups.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 _DUPS_FILE="_dups.txt"
 
-./compare_img.py $* | sed \
+_home="`dirname "$0"`"
+
+python "${_home}/compare_img.py" $* | sed \
 	-e 's/^.*-\([0-9]\+\).*-\([0-9]\+\).*$/\1\t\2/' \
 	-e '/^\([0-9]\+\)\t\1$/d' | sort | uniq | tee ${_DUPS_FILE}
